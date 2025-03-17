01c8160fb2ffa479baba19643c26d9e1875a73b5
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-03-17 12:24:10 +0100

    Add: exercise complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-
+import DefaultLayout from './layout/DefaultLayout'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Films from './pages/Films'
+import SingleFilm from './pages/SingleFilm'
 
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path='/' Component={Films} />
+            <Route path='/films/:id' Component={SingleFilm} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/SingleFilm.jsx
@@ -0,0 +1,5 @@
+export default function SingleFilm() {
+    return(
+        <h2>Film specifico</h2>
+    )
+}
